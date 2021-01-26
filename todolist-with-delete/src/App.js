3b13d1828c7bf5b144263e7e79d10b82262b2ea0
Sasu Korhonen <smage90@hotmail.com>
2021-01-26 15:51:12 +0200

    exercise 13

---
+++ todolist-with-delete/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [todo, setTodo] = useState({date: '', desc: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name] : event.target.value});
+  }
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const deleteTodo = (event) => {
+    event.preventDefault();
+    setTodos(todos.filter((item, index) => index !== parseInt(event.target.value)));
+  }
+
+  return (
+    <div>
+      <div className="App-header">
+        <h1>To do list</h1>
+      </div>
+      <div className="App">
+        <form onSubmit={addTodo}> 
+          <input type="text" name="date" value={todo.date} onChange={inputChanged}/>
+          <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+          <input type="submit" value="Add"/>
+        </form>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo, index) => 
+              <tr key={index}>
+                <td>{todo.date}</td>
+                <td>{todo.desc}</td>
+                <td><button onClick={deleteTodo} value={index}>Delete</button></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
