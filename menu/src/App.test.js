9d9fd9590fc5e987cf1fb64fe738b0baa1da0710
Sasu Korhonen <smage90@hotmail.com>
2021-03-01 10:24:39 +0200

    Exercise 22

--- menu/src/App.test.js
+++ menu/src/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import TodoTable from './components/TodoTable';
+import '@testing-library/jest-dom/extend-expect';
+import Todos from './components/Todos';
+import { render, fireEvent } from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+test('renderstodotable', () =>  {
+  const row = [{desc: 'Goto  coffee', date:'24.11.2019'}]
+  const todotable = render(<TodoTable todos={row}/>);
+
+  expect(todotable.container). toHaveTextContent('Goto coffee');
+  expect(todotable.container).not.toHaveTextContent('Some text');
+})
+
+test('addtodo', () => {
+  const { container, getByText, getByPlaceholderText } = render(<Todos/> );
+
+  const desc = getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value:'Go to coffee' }})
+
+  const date = getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value:'29.11.2019' }})
+
+  const button= getByText('Add');
+  fireEvent.click(button);
+
+  expect(container).toHaveTextContent('Go to coffee');
+})
--- menu/src/components/Todos.js
+++ menu/src/components/Todos.js
@@ -2,48 +2,36 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import MomentUtils from '@date-io/moment';
-import {
-  DatePicker,
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 import "moment/locale/fi";
 import moment from 'moment';
+import TodoTable from './TodoTable';
 
 
-function Todos() {
-    const [todo, setTodo] = useState({desc: '', date: moment().format('l')});
-    const [todos, setTodos] = useState([]);
-    const [date, setDate] = useState(moment());
-    const locale = 'fi';
+export default function Todos() {
+  const [todo, setTodo] = useState({desc: '', date: moment().format('l')});
+  const [todos, setTodos] = useState([]);
+  const [date, setDate] = useState(moment());
+  const locale = 'fi';
 
-    const addTodo = (event) => {
-        event.preventDefault();
-        setTodos([...todos, todo]);
-    }
-    
-      const inputChanged = (event) => {
-        setTodo({...todo, [event.target.name]: event.target.value});
-    }
-    
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+  
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+  
 
-    return (
-        <div style={{marginTop: '1vh'}}>
-        <MuiPickersUtilsProvider utils={MomentUtils} locale={locale}>
-          <DatePicker label="Date" value={date} onChange={newDate =>  setTodo({...todo, date: newDate.format('l')}, setDate(newDate))}/>
-        </MuiPickersUtilsProvider> 
-        <TextField label="Description" name="desc" value={todo.desc} onChange={inputChanged}/>
-        <Button style={{marginTop: '1vh'}}variant="contained" color="primary" onClick={addTodo}>Add</Button>
-        <table>
-          <tbody>
-            {todos.map((todo, index) => 
-              <tr key={index}>
-                <td>{todo.date}</td>
-                <td>{todo.desc}</td>
-              </tr>)}
-          </tbody>
-        </table>
-      </div>
-    )
+  return (
+    <div style={{marginTop: '1vh'}}>
+      <MuiPickersUtilsProvider utils={MomentUtils} locale={locale}>
+        <DatePicker placeholder="Date" label="Date" value={date} onChange={newDate =>  setTodo({...todo, date: newDate.format('l')}, setDate(newDate))}/>
+      </MuiPickersUtilsProvider> 
+      <TextField placeholder="Description" label="Description" name="desc" value={todo.desc} onChange={inputChanged}/>
+      <Button style={{marginTop: '1vh'}}variant="contained" color="primary" onClick={addTodo}>Add</Button>
+      <TodoTable todos={todos}/>
+    </div>
+  )
 }
-
-export default Todos;
